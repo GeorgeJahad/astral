58f435761af4108e2b45a6926345b7e6d5a89b5a
George Jahad <george@georgejahad.com>
2024-10-04 15:10:43 -0700

    updated latest

--- test/lib/clients/vault_test.rb
+++ test/lib/clients/vault_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+#require "test_helper"
 
 class VaultTest < ActiveSupport::TestCase
   attr_reader :intermediate_ca_mount
@@ -7,6 +7,8 @@ class VaultTest < ActiveSupport::TestCase
   attr_reader :policies
   attr_reader :entity_name
   attr_reader :alias_name
+  attr_reader :client_id
+  attr_reader :client_secret
 
   setup do
     @client = Clients::Vault
@@ -110,7 +112,99 @@ class VaultTest < ActiveSupport::TestCase
     assert_match /no such alias/, err.message
   end
 
+  def configure_oidc_provider
+    oidc_provider.logical.delete("/sys/auth/userpass")
+    oidc_provider.logical.write("/sys/auth/userpass", type: "userpass")
+    oidc_provider.logical.write("auth/userpass/users/end-user", password: "securepassword")
+
+    # create oidc provider app
+    oidc_provider.logical.write("identity/oidc/client/my-webapp",
+                               redirect_uris: "http://localhost:8250/oidc/callback",
+                               assignments: "allow_all")
+
+    app = oidc_provider.logical.read("identity/oidc/client/my-webapp")
+    @client_id = app.data[:client_id]
+    @client_secret = app.data[:client_secret]
+    oidc_provider.logical.write("identity/oidc/scope/email",
+                               template: '{"email": {{identity.entity.metadata.email}}}')
+
+    oidc_provider.logical.write("identity/oidc/provider/my-provider",
+                               issuer: "http://oidc_provider:8300",
+                               allowed_client_ids: @client_id,
+                               scopes_supported: "email")
+    oidc_provider.logical.write("identity/entity",
+                               policies: "default",
+                               name: "end-user",
+                               metadata: "email=vault@hashicorp.com",
+                               disabled: false)
+    provider = oidc_provider.logical.read("identity/oidc/provider/my-provider")
+
+    op_entity = oidc_provider.logical.read("identity/entity/name/end-user")
+    op_entity_id = op_entity.data[:id]
+    op_auth_list = oidc_provider.logical.read("/sys/auth")
+    up_accessor = op_auth_list.data[:"userpass/"][:accessor]
+    oidc_provider.logical.write("identity/entity-alias",
+                               name: "end-user",
+                               canonical_id: op_entity_id,
+                               mount_accessor: up_accessor)
+  end
+
+  def configure_oidc_client
+    #configure oidc client
+    vault_client.logical.delete("/sys/auth/oidc")
+    vault_client.logical.write("/sys/auth/oidc", type: "oidc")
+    vault_client.logical.write("auth/oidc/config",
+                               oidc_discovery_url: "http://oidc_provider:8300/v1/identity/oidc/provider/my-provider",
+                               oidc_client_id: @client_id,
+                               oidc_client_secret: @client_secret,
+                               default_role: "reader")
+    policy = <<-EOH
+      path "sys" {
+      policy = "deny"
+    }
+    EOH
+    vault_client.sys.put_policy("reader", policy)
+    vault_client.logical.write("auth/oidc/role/reader",
+                               bound_audiences: @client_id,
+                               allowed_redirect_uris: "http://localhost:8200/ui/vault/auth/oidc/oidc/callback,http://localhost:8250/oidc/callback,http://127.0.0.1:8200/ui/vault/auth/oidc/oidc/callback,http://127.0.0.1:8250/oidc/callback",
+                               user_claim: "email",
+                               oidc_scopes: "email",
+                               token_policies: "reader")
+  end
+
+  def test_oidc
+    configure_oidc_provider
+    configure_oidc_client
+    policy = <<-EOH
+      path "sys" {
+      policy = "deny"
+    }
+    EOH
+    vault_client.sys.put_policy("manager", policy)
+
+    vault_client.logical.write("identity/entity",
+                               policies: "manager",
+                               name: "end-user",
+                               disabled: false)
+    entity = vault_client.logical.read("identity/entity/name/end-user")
+    entity_id = entity.data[:id]
+    auth_list = vault_client.logical.read("/sys/auth")
+    oidc_accessor = auth_list.data[:"oidc/"][:accessor]
+    vault_client.logical.write("identity/entity-alias",
+                               name: "vault@hashicorp.com",
+                               canonical_id: entity_id,
+                               mount_accessor: oidc_accessor)
+
+  end
+
   private
+
+  def oidc_provider
+    ::Vault::Client.new(
+          address: "http://oidc_provider:8300",
+          token: "root_token"
+    )
+  end
 
   def vault_client
     ::Vault::Client.new(
